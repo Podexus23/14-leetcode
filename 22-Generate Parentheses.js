0f359828c6759ec1e6e7c1770ee3c480f25a62da
Podexus23 <56443118+Podexus23@users.noreply.github.com>
2023-09-13 22:42:32 +0300

    Update 22-Generate Parentheses.js

--- 22-Generate Parentheses.js
+++ 22-Generate Parentheses.js
@@ -3,35 +3,27 @@
  * @return {string[]}
  */
 
-function addParent(str) {
-  return `(${str})`;
-}
-
-function createCombinations(num) {
-  if (num === 2) return `${num}`;
-  if (num === 1) return `${num}`;
-  const res = [];
-  for (let i = 1; i <= num; i++) {}
-  return res;
-}
-
 var generateParenthesis = function (n) {
-  let counter = 0;
-  const base = "()";
-  const children = [];
-  const stack = {
-    layer: n,
-    open: [],
-    close: [],
-  };
-  while (counter < n) {
-    counter++;
-  }
-  return children;
-};
+  const stack = [];
+  const res = [];
 
-console.log(generateParenthesis(5)); //["(((())))","((()()))","((())())","(()(()))", "(()()())", "()((()))", "((()))()", "(())(())", "()()()()"], [5, 3(11), 2(111), 2(21), 2(12), 2(111), 1(31), 1(13) 1(22), 1(112), 1(211), 1(1111)]
-console.log(generateParenthesis(4)); //["(((())))","((()()))","((())())","(()(()))", "(()()())", "()((()))", "((()))()", "(())(())", "()()()()"], [3(1), 2(11), (21), (12), (111), 31, 13, 22, 112, 211, 121, 1111]
-console.log(generateParenthesis(3)); //["((()))","(()())","(())()","()(())","()()()"]                                                             [1(2), 1(11), 21, 12, 111]
-console.log(generateParenthesis(2)); //["(())","()()"]                                                                                            [2, 11]
-console.log(generateParenthesis(1)); //["()"]                                                                                                     [1]
+  function createCombinations(open, close) {
+    if (open == close && open == n && close == n) {
+      res.push(stack.join(""));
+      return;
+    }
+    if (open < n) {
+      stack.push("(");
+      createCombinations(open + 1, close);
+      stack.pop();
+    }
+    if (close < open) {
+      stack.push(")");
+      createCombinations(open, close + 1);
+      stack.pop();
+    }
+  }
+
+  createCombinations(0, 0);
+  return res;
+};                                                            
